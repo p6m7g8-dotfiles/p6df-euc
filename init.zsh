1806454e5a0d6c51939c1fb779a79c9144b82adb
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-02 23:47:24 -0400

    fix(init): remove broken profile::mod function with empty variable reference (#3)

--- init.zsh
+++ init.zsh
@@ -11,20 +11,3 @@ p6df::modules::euc::deps() {
     p6m7g8-dotfiles/p6df-aws
   )
 }
-
-######################################################################
-#<
-#
-# Function: words euc $EUC_HOST = p6df::modules::euc::profile::mod()
-#
-#  Returns:
-#	words - euc $EUC_HOST
-#
-#  Environment:	 EUC_HOST
-#>
-######################################################################
-p6df::modules::euc::profile::mod() {
-
-  p6_return_words 'euc' "$"
-}
-
